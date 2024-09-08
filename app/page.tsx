ac52ec04d8a803903dc9b9104c18ba074a0077b4
Charles Parker <charles.parker@mdpanel.com>
2024-09-07 19:50:20 -0700

    finished tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@
 
 import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotPopup } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
   return (
-    <TasksProvider>
-      <TasksList />
-    </TasksProvider>
+    <CopilotKit publicApiKey={"ck_pub_7c31d25212559c6cb212797018c2326b"}>
+      <TasksProvider>
+        <TasksList />
+      </TasksProvider>
+      <CopilotPopup />
+    </CopilotKit>
   );
 }
